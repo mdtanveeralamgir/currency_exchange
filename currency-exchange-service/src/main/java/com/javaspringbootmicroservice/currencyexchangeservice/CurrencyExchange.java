96552161ebd73909af38750484cf154ec08b28fd
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-04 23:13:34 -0400

    add port properties in bean so displaying which port is being used for a instance of exchange service, edit configuration to launch the appication in different port (details in readme)

--- currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CurrencyExchange.java
@@ -7,7 +7,10 @@ public class CurrencyExchange {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
-
+    //identifies which instances of currencyExchange has been providing the reponse by port number
+    //If port is 8000 then instance 1
+    //If port is 8001 then instance 2 and so on
+    private String environment;
     public CurrencyExchange(){}
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
@@ -47,6 +50,14 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 
     @Override
--- currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.javaspringbootmicroservice.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,17 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment; //Spring boot offers environment class to get the environment properties
     @GetMapping("/currency_exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExhangeValue(
             @PathVariable String from,
             @PathVariable String to
             ){
-        return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+        //Get the port of the CurrencyExchange in use from environment
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
