9ca92d36c43d6e884a29f3e6e0361d13f33623b6
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-05 23:16:04 -0400

    calling CurrencyExchange service using rest api method of spring, get value and pass to currencyConversion bean and display the converted currency

--- currency-conversion-service/src/main/java/com/java/springboot/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/java/springboot/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,18 +1,29 @@
 package com.java.springboot.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
 
     @GetMapping("currency_conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion retrieveConvertedValue(@PathVariable String from, @PathVariable String to, BigDecimal quantity)
+    public CurrencyConversion retrieveConvertedValue(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)
     {
-        //Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculatedAmount, String environment
-        return new CurrencyConversion(1001L, from, to, BigDecimal.valueOf(10), BigDecimal.valueOf(56), BigDecimal.valueOf(100), "8100");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        //Calling a rest api
+        //from and to will be passed by hashmap (uriVariables)
+        //the properties name of CurrencyExchange bean should be same as CurrencyConversion properties in order to map properly
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8001/currency_exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
     }
 }
--- currency-conversion-service/src/main/java/com/java/springboot/microservices/currencyconversionservice/CurrencyConversionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.java.springboot.microservices.currencyconversionservice;
-//import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigDecimal;
-
-//public interface CurrencyConversionRepository extends JpaRepository<CurrencyConversion, Long> {
-//}
