bdaaf4b3de0adc778c82438c05cc79354eabfb42
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-07 22:26:04 -0400

    ratelimit: certan request in certain amount of time, bulkhead: max concurrent calls

--- currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/javaspringbootmicroservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.javaspringbootmicroservice.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +16,15 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")//retry upon failure using default configuration
-    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodedResponse")
+//    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    @RateLimiter(name = "default") //certain number of requests are allowed in certain period of time
+    @Bulkhead(name = "default")
     public String sampleApi()
     {
         logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/intend-to-fail", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/intend-to-fail", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     //After the retry shows this response in the page
